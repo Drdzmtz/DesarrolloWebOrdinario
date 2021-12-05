aa0c12ae5cc66c71f60888a594bd9aa1b82ed97c
Danieldm16 <salazar_daniel@hotmail.es>
2021-12-04 23:06:40 -0600

    Models of Houses and News

---
+++ app/models/House.py
@@ -0,0 +1,18 @@
+class House():
+    
+     def __init__(self, id, photo, city, state, zip_code, 
+     price, rooms, bathrooms, longitude, latitude, description,
+     status, type):
+        self.id = id
+        self.photo = photo
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.price = price
+        self.rooms = rooms
+        self.bathrooms = bathrooms
+        self.longitude = longitude
+        self.latitude = latitude
+        self.description = description
+        self.status = status
+        self.type = type
---
+++ app/models/News.py
@@ -0,0 +1,6 @@
+class News():
+    
+     def __init__(self, id, title, description):
+        self.id = id
+        self.photo = title
+        self.city = description
