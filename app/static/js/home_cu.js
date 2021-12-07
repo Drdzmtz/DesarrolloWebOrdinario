d7dd0a6352be9e0e1c90b5b625ce7d2ec81be210
David Aguirre <daguirrez2000@gmail.com>
2021-12-07 04:47:17 -0600

    Added checks in home_ul form page

--- app/static/js/home_cu.js
+++ app/static/js/home_cu.js
@@ -4,10 +4,21 @@ window.addEventListener('load', () => {
 		ev.preventDefault();
 		
 		const data = {};
-		const inputs = Array.from(ev.target.querySelectorAll('input, textarea, select'));
+		const errors = [];
+		const inputs = Array.from(ev.target.querySelectorAll('input:not([type="file"]), textarea, select'));
 
-		for(let ipt of inputs)
+		for(let ipt of inputs) {
 			data[ipt.name] = ipt.value.trim();
+
+			if (ipt.name === 'id') continue;
+			if (data[ipt.name] === '')
+				errors.push(`El campo ${ipt.dataset.realName} se encuentra vacío.\n`);
+		}
+
+		if (errors.length > 0) {
+			alert(''.concat(...errors))
+			return;
+		}
 			
 		// get photo
 		const file = document.getElementById('fphoto').files[0];
@@ -20,8 +31,8 @@ window.addEventListener('load', () => {
 			async: true,
 			data,
 			success(res) {
-				if (res.error) {
-					alert(res.error);
+				if (res.Error) {
+					alert(res.Error);
 					return;
 				}
 
