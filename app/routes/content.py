1ba653cc1b899ff4de0f5c1e3a3579cf4add0d1a
David Aguirre <daguirrez2000@gmail.com>
2021-12-07 02:32:52 -0600

    feat: :lipstick: Added pages of content integration with the main page

--- app/routes/content.py
+++ app/routes/content.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 content_routes = Blueprint("content_routes", __name__)
 
 @content_routes.route("/requisitos")
 def requirements():
-    return "REQUESITOS"
+    return render_template('requirements.html')
+
+@content_routes.route("/servicios")
+def services():
+    return render_template('services.html')
+
+@content_routes.route("/nosotros")
+def company():
+    return render_template('company.html')
+
+@content_routes.route("/mudanzas")
+def moving():
+    return render_template('moving.html')
+
+@content_routes.route("/seguros")
+def insurances():
+    return render_template('insurances.html')
